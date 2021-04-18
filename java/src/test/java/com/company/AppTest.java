7ccb1ca35d9f28bfc9e59a313f9aa87140f17f76
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-18 19:38:28 +0100

    updsates test api to use Junit 5

--- java/src/test/java/com/company/AppTest.java
+++ java/src/test/java/com/company/AppTest.java
@@ -1,8 +1,8 @@
 package com.company;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
