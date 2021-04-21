7ab72552c5457a9cb7155ef694772e51a9a71229
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-21 16:10:39 +0100

    creates predicate app

--- java/src/main/java/com/company/function/PredicateApp.java
+++ java/src/main/java/com/company/function/PredicateApp.java
@@ -1,2 +1,12 @@
-package com.company.function;public class PredicateApp {
+package com.company.function;
+
+import java.util.function.Predicate;
+
+public class PredicateApp {
+
+    public static void main(String[] args) {
+        Predicate<String> startWithA = s -> s.startsWith("A");
+        Predicate<String> startWithB = s -> s.startsWith("B");
+        System.out.println(startWithA.and(startWithB).test("Animal"));
+    }
 }
