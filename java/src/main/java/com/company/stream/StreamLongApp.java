1d877043cbfd78948d6645e77433a90deb9ab5d0
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-21 15:57:59 +0100

    creates stream app long

--- java/src/main/java/com/company/stream/StreamLongApp.java
+++ java/src/main/java/com/company/stream/StreamLongApp.java
@@ -1,15 +1,17 @@
 package com.company.stream;
 
 import java.util.IntSummaryStatistics;
+import java.util.LongSummaryStatistics;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
-public class StreamIntApp {
+public class StreamLongApp {
 
     public static void main(String[] args) {
-        IntStream stream = IntStream.of(1, 2, 3, 4);
-        IntSummaryStatistics statistics = stream.summaryStatistics();
-        int min = statistics.getMin();
-        int max = statistics.getMax();
+        LongStream stream = LongStream.of(1, 2, 3, 4);
+        LongSummaryStatistics statistics = stream.summaryStatistics();
+        long min = statistics.getMin();
+        long max = statistics.getMax();
         double average = statistics.getAverage();
         long count = statistics.getCount();
         long sum = statistics.getSum();
