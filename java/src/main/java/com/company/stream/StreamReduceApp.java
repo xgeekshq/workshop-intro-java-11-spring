aff61db54be6dd096671bd44619ed01c2444216b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-21 11:52:47 +0100

    creates stream sample reduce app

--- java/src/main/java/com/company/stream/StreamReduceApp.java
+++ java/src/main/java/com/company/stream/StreamReduceApp.java
@@ -1,4 +1,14 @@
 package com.company.stream;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 public class StreamReduceApp {
+
+    public static void main(String[] args) {
+        List<BigDecimal> values = List.of(BigDecimal.ONE, BigDecimal.TEN);
+        Optional<BigDecimal> total = values.stream().reduce(BigDecimal::add);
+        System.out.println(total);
+    }
 }
