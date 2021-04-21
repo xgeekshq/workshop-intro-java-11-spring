c4a9f5adc8a3d2e0773c60d4d5a05363d63fa987
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-21 15:57:47 +0100

    creates stream int app

--- java/src/main/java/com/company/stream/StreamIntApp.java
+++ java/src/main/java/com/company/stream/StreamIntApp.java
@@ -1,2 +1,17 @@
-package com.company.stream;public class StreamIntApp {
+package com.company.stream;
+
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+
+public class StreamIntApp {
+
+    public static void main(String[] args) {
+        IntStream stream = IntStream.of(1, 2, 3, 4);
+        IntSummaryStatistics statistics = stream.summaryStatistics();
+        int min = statistics.getMin();
+        int max = statistics.getMax();
+        double average = statistics.getAverage();
+        long count = statistics.getCount();
+        long sum = statistics.getSum();
+    }
 }
