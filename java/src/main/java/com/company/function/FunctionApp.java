51d19c8e62aedaf18fe24142d5edc4f7d9865176
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-21 16:10:31 +0100

    creates funcion app

--- java/src/main/java/com/company/function/FunctionApp.java
+++ java/src/main/java/com/company/function/FunctionApp.java
@@ -1,2 +1,16 @@
-package com.company.function;public class FunctionApp {
+package com.company.function;
+
+import java.util.function.Function;
+
+public class FunctionApp {
+
+    public static void main(String[] args) {
+        Function<String, Integer> toNumber = Integer::parseInt;
+        System.out.println("To number: " + toNumber.apply("234"));
+        Function<String, String> upperCase = String::toUpperCase;
+        Function<String, String> trim = String::trim;
+        Function<String, String> searchEngine = upperCase.andThen(trim);
+        System.out.println("Search result: " + searchEngine.apply("   test one two   "));
+
+    }
 }
