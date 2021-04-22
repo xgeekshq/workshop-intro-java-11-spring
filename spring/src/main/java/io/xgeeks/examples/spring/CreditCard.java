93ae4a90ab173fc1b87d3c52b76025e981b60179
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-22 13:07:27 +0100

    adds payment sample

--- spring/src/main/java/io/xgeeks/examples/spring/CreditCard.java
+++ spring/src/main/java/io/xgeeks/examples/spring/CreditCard.java
@@ -1,2 +1,7 @@
-package io.xgeeks.examples.spring;public class CreditCard {
+package io.xgeeks.examples.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreditCard implements Payment {
 }
--- spring/src/main/java/io/xgeeks/examples/spring/SpringApp.java
+++ spring/src/main/java/io/xgeeks/examples/spring/SpringApp.java
@@ -1,2 +1,16 @@
-package io.xgeeks.examples.spring;public class SpringApp {
+package io.xgeeks.examples.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class SpringApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringApp.class);
+        Payment payment = context.getBean(Payment.class);
+        System.out.println(payment);
+
+    }
 }
