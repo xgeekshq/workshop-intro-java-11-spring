7f7ec561ee2c8b817afac16c94542a4f3fba0299
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-23 14:07:03 +0100

    date api

--- java/src/main/java/com/company/date/DateApp.java
+++ java/src/main/java/com/company/date/DateApp.java
@@ -1,13 +1,20 @@
 package com.company.date;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.YearMonth;
 
 public class DateApp {
 
     public static void main(String[] args) {
         System.out.println("LocalDateTime: " + LocalDateTime.now());
         System.out.println("Localdate: " + LocalDate.now());
+        System.out.println("LocalDateTime: " + LocalDateTime.now());
+        System.out.println("YearMonth: " + YearMonth.now());
+        System.out.println("Year: " + Year.now());
+        System.out.println("Days of weeks: " + DayOfWeek.values());
     }
 }
 
