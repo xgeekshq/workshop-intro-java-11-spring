5833e764a78de6ec7520e2480bba059d4c4a9bfc
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-23 19:56:32 +0100

    ignores mehtod in the ticket

--- spring-data/src/main/java/io/xgeeks/examples/spring/user/BusTicket.java
+++ spring-data/src/main/java/io/xgeeks/examples/spring/user/BusTicket.java
@@ -1,11 +1,15 @@
 package io.xgeeks.examples.spring.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("BUS")
 public class BusTicket extends Ticket {
+
+    @JsonIgnore
     @Override
     public TicketType getType() {
         return TicketType.BUS;
--- spring-data/src/main/java/io/xgeeks/examples/spring/user/PlaneTicket.java
+++ spring-data/src/main/java/io/xgeeks/examples/spring/user/PlaneTicket.java
@@ -1,11 +1,15 @@
 package io.xgeeks.examples.spring.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("PLANE")
 public class PlaneTicket extends Ticket {
+
+    @JsonIgnore
     @Override
     public TicketType getType() {
         return TicketType.PLAIN;
