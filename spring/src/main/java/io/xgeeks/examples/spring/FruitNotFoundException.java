993dd90f625763f8ad880272ab1095cb4c698605
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-23 06:56:54 +0100

    creates fruit models

--- spring/src/main/java/io/xgeeks/examples/spring/FruitNotFoundException.java
+++ spring/src/main/java/io/xgeeks/examples/spring/FruitNotFoundException.java
@@ -1,2 +1,12 @@
-package io.xgeeks.examples.spring;public class FruitNotFoundException {
+package io.xgeeks.examples.spring;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FruitNotFoundException extends RuntimeException {
+
+    public FruitNotFoundException(String message) {
+        super(message);
+    }
 }
