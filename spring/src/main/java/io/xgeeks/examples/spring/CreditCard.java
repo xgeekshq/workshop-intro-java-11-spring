06c89c35847d292d38867f87bc29d531ff57d63c
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-23 06:56:39 +0100

    adds payment specializations

--- spring/src/main/java/io/xgeeks/examples/spring/CreditCard.java
+++ spring/src/main/java/io/xgeeks/examples/spring/CreditCard.java
@@ -3,5 +3,5 @@ package io.xgeeks.examples.spring;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DebitCard implements Payment {
+public class CreditCard implements Payment {
 }
