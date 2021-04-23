5ea1f3a682490b46d7fd7b55a5f7ac79eb40d500
Otavio Santana <otaviopolianasantana@gmail.com>
2021-04-23 07:06:25 +0100

    removes imports

--- spring/src/main/java/io/xgeeks/examples/spring/PaymentService.java
+++ spring/src/main/java/io/xgeeks/examples/spring/PaymentService.java
@@ -1,9 +1,7 @@
 package io.xgeeks.examples.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class PaymentService {
