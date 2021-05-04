c1ab496c7e48fe383770ba0c312a6c58d967cfb5
Otavio Santana <otaviopolianasantana@gmail.com>
2021-05-04 16:53:42 +0100

    creates validation exception handler

---
+++ spring-data/src/main/java/io/xgeeks/examples/spring/ValidationExceptionHandler.java
@@ -0,0 +1,29 @@
+package io.xgeeks.examples.spring;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handle(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        return errors;
+    }
+}
